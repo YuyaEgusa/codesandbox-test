9c6f78f181f2a21575ebb6648c7ba2d189513dbe
YuyaEgusa <80240539+YuyaEgusa@users.noreply.github.com>
2021-05-05 10:48:01 +0900

    途中保存

--- src/index.js
+++ src/index.js
@@ -88,14 +88,111 @@
 /**
  * 分割代入
  */
-const myProfile = {
-  name: "じゃけぇ",
-  age: 28
-};
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
 
-const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
-console.log(message1);
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
 
-const { name, age } = myProfile;
-const message2 = `名前は${name}です。年齢は${age}歳です。`;
-console.log(message2);
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = ["じゃけぇ", 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん`);
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+//  配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1);
+// // console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]); //1 2
+// sumFunc(...arr1);  //1 2(arr1の値を順番に処理する)
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4]; //arr4のコピー
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4,...arr5]; //arr4,arr5の結合
+// console.log(arr7);
+
+//＝で配列をコピーすると、配列に変更を与えた際に参照先の配列に影響を与えてしまう
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理（どっちもアロー関数を使う）
+ */
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let i = 0; i < nameArr.length; i++) {
+//   console.log(`${i + 1}番目は${nameArr[i]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const numArr = [1,2,3,4,5];
+// const newNumArr = numArr.filter((num) =>{
+//   return num % 2 === 1;
+// })
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+const num = "1300";
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+const check
